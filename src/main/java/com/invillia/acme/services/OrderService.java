3e58c08dde569bd2a6c4d442d8a1ca3063742f81
Lucas de Abreu <lucasdeabreu@gmail.com>
2018-12-21 19:06:12 -0200

    Add Order service

---
+++ src/main/java/com/invillia/acme/services/OrderService.java
@@ -0,0 +1,4 @@
+package com.invillia.acme.services;
+
+public interface OrderService {
+}
---
+++ src/main/java/com/invillia/acme/services/OrderServiceImpl.java
@@ -0,0 +1,14 @@
+package com.invillia.acme.services;
+
+import com.invillia.acme.repositories.OrderRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderServiceImpl implements OrderService {
+
+    private final OrderRepository orderRepository;
+
+    public OrderServiceImpl(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+}
