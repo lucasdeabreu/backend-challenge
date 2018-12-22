f09d1d3cdb00c9fb3e05eeee0fb6a26da9b5160a
Lucas de Abreu <lucasdeabreu@gmail.com>
2018-12-21 22:01:32 -0200

    Replace @Profile for @ActiveProfiles

--- src/test/java/com/invillia/acme/controllers/OrderControllerTestIT.java
+++ src/test/java/com/invillia/acme/controllers/OrderControllerTestIT.java
@@ -9,8 +9,8 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.context.annotation.Profile;
 import org.springframework.http.*;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -19,7 +19,7 @@ import java.util.Objects;
 
 import static org.junit.Assert.assertEquals;
 
-@Profile("test")
+@ActiveProfiles("test")
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class OrderControllerTestIT {
