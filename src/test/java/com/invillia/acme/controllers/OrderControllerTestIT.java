1816e5e0ef9e63eaaa24b21a74d45744eed94335
Lucas de Abreu <lucasdeabreu@gmail.com>
2018-12-21 22:00:21 -0200

    Add profiles prod, dev, and test

--- src/test/java/com/invillia/acme/controllers/OrderControllerTestIT.java
+++ src/test/java/com/invillia/acme/controllers/OrderControllerTestIT.java
@@ -9,6 +9,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.*;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -18,6 +19,7 @@ import java.util.Objects;
 
 import static org.junit.Assert.assertEquals;
 
+@Profile("test")
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class OrderControllerTestIT {
