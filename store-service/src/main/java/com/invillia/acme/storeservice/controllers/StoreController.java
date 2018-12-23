7924901c486c144a823b67d55f10b87148e8f36c
Lucas de Abreu <lucasdeabreu@gmail.com>
2018-12-23 20:44:18 -0200

    Add Stores endpoints

--- store-service/src/main/java/com/invillia/acme/storeservice/controllers/StoreController.java
+++ store-service/src/main/java/com/invillia/acme/storeservice/controllers/StoreController.java
@@ -1,14 +1,22 @@
 package com.invillia.acme.storeservice.controllers;
 
+import com.invillia.acme.storeservice.dtos.StoreDto;
 import com.invillia.acme.storeservice.dtos.mappers.StoreDtoMapper;
+import com.invillia.acme.storeservice.exceptions.StoreNotFound;
 import com.invillia.acme.storeservice.services.StoreService;
 import io.swagger.annotations.Api;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import javax.validation.Valid;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/stores")
-@Api(value="store", description="Actions related to Store Service")
+@Api(value = "store", description = "Actions related to Store Service")
 public class StoreController {
 
     private final StoreService storeService;
@@ -19,7 +27,37 @@ public class StoreController {
         this.mapper = mapper;
     }
 
+    @ApiOperation(value = "Create a Store")
+    @PostMapping
+    public ResponseEntity create(@RequestBody @Valid StoreDto dto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(
+                mapper.storeToDto(storeService.save(mapper.dtoToStore(dto)))
+        );
+    }
 
+    @ApiOperation(value = "Update a Store")
+    @PutMapping("/{storeId}")
+    public ResponseEntity update(@PathVariable Long storeId, @RequestBody @Valid StoreDto dto) {
+        try {
+            return ResponseEntity.status(HttpStatus.OK).body(
+                    mapper.storeToDto(storeService.update(storeId, mapper.dtoToStore(dto)))
+            );
+        } catch (StoreNotFound e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage(), e);
+        }
+    }
 
-
+    @ApiOperation(value = "Find Store by parameters")
+    @GetMapping
+    public ResponseEntity findByParameters(
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false) String address) {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(
+                        storeService.findAllByParameters(name, address)
+                                .stream()
+                                .map(mapper::storeToDto)
+                                .collect(Collectors.toList())
+                );
+    }
 }
