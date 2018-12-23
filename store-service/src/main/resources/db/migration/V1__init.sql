182985708fff85da89d4cf848e0767dceb3006fa
Lucas de Abreu <lucasdeabreu@gmail.com>
2018-12-23 21:02:11 -0200

    Add Flyway

---
+++ store-service/src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,6 @@
+create table stores (
+       id bigint not null AUTO_INCREMENT,
+       name varchar(255) not null,
+        address varchar(255) not null,
+        primary key (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
