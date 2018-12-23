d4131594660bcefdb5e25f8e010e4cf9367d898a
Lucas de Abreu <lucasdeabreu@gmail.com>
2018-12-23 19:19:32 -0200

    Remove unused import

--- order-service/src/main/java/com/invillia/acme/dtos/OrderDto.java
+++ order-service/src/main/java/com/invillia/acme/dtos/OrderDto.java
@@ -1,10 +1,12 @@
 package com.invillia.acme.dtos;
 
 import com.invillia.acme.entities.OrderStatus;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.Valid;
-import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
